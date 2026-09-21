98e41955d8792a84c7c11e8543458114244b5796
Almeida <neaa01111@gmail.com>
2026-09-21 20:04:39 -0300

    Update percorrer clientes 2.sql

---
+++ exercicos/percorrer clientes 2.sql
@@ -0,0 +1,29 @@
+--CTE: COMMON TABLE EXPRESSION
+-- Dos clientes que comecaram sql no primeiro dia quantos chegaram ao 5 Dia
+
+WITH tb_cliente_primeiro_dia AS (
+    SELECT DISTINCT IDcliente
+    from transacoes
+    WHERE substr(Dtcriacao,1,10) = '2025-08-25'
+),
+
+tb_cliente_ultimo_dia AS (
+    SELECT DISTINCT IDcliente
+    from transacoes
+    WHERE substr(Dtcriacao,1,10) = '2025-08-29'
+),
+
+tb_join AS (
+    SELECT t1.Idcliente AS PrimeiroDia,
+    t2.Idcliente AS UltimoDia
+
+    FROM tb_cliente_primeiro_dia AS t1
+
+    LEFT JOIN tb_cliente_ultimo_dia AS t2
+    ON t1.Idcliente = t2.Idcliente
+)
+
+SELECT count(PrimeiroDia) As ClientesPrimeiroDia,
+        count(UltimoDia) AS ClientesUltimoDia
+
+FROM tb_join
